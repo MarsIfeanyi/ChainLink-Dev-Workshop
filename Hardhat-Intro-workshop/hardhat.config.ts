bb83675b4be725e578fedd27278e94869bafba60
MarsIfeanyi <marschalice@gmail.com>
2023-04-29 23:15:31 +0100

    Initial Commit

---
+++ Hardhat-Intro-workshop/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@chainlink/hardhat-chainlink";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
